7b80137ea389aaae09f0fd0468f1d9c5d23aeb8a
AN KYOUNG EUN <7588124@naver.com>
2020-11-20 10:40:11 +0900

    Update malloc01.c

--- malloc01.c
+++ malloc01.c
@@ -23,5 +23,4 @@ int main()
 }
 
 
-//The name heap has nothing to do with heap data structure. 
-//It is called heap because it is a pile of memory space available to programmers to allocated and de-allocate.
+
